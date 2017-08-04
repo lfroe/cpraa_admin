9d8a9bcfd4b1800dc1769378056ee552b6f75c6c
Lukas Froeschl <lukas.froeschl@dig.at>
2017-08-04 18:23:57 +0200

    *test

--- app/services/scheduleEntryService.js
+++ app/services/scheduleEntryService.js
@@ -2,15 +2,14 @@
  * Created by lukas on 09/04/2017.
  */
 const ScheduleEntry = require('../models/scheduleEntry');
-let moment = require('moment');
+const moment = require('moment');
 
 module.exports = {
     saveScheduleEntry: async (requestData, user) => {
-        console.log('semaphore test');
         requestData.owner = requestData.user;
         requestData.start = moment.utc(requestData.start).format();
         requestData.end = moment.utc(requestData.end).format();
-        scheduleEntry = await new ScheduleEntry({
+        const scheduleEntry = await new ScheduleEntry({
             start: requestData.start,
             schoolClassId: requestData.schoolClassId,
             end: requestData.end, owner: user, eventId: requestData.eventId,
@@ -19,7 +18,7 @@ module.exports = {
         return {scheduleEntry: scheduleEntry}
     },
     updateScheduleEntry: async (id, requestData) => {
-        let scheduleEntry = await ScheduleEntry.findOne({_id: id});
+        const scheduleEntry = await ScheduleEntry.findOne({_id: id});
         if (!scheduleEntry) {
             return {success: false, msg: 'ScheduleEntry not found'};
         }
@@ -38,9 +37,9 @@ module.exports = {
         return {success: true};
     },
     deleteScheduleEntry: async (id) => {
-        let entry = await ScheduleEntry.findOne({_id: id});
+        const entry = await ScheduleEntry.findOne({_id: id});
         if (!entry) {
-            return {success: false, msg: "ScheduleEntry not found"};
+            return {success: false, msg: 'ScheduleEntry not found'};
         }
         await entry.remove();
         return {success: true};
@@ -56,7 +55,7 @@ module.exports = {
         return {success: true, entries: scheduleEntries}
     },
     findByDateRange: async (startDate, endDate) => {
-        let entries = await ScheduleEntry.find({
+        const entries = await ScheduleEntry.find({
             $and: [
                 {start: {$gt: startDate}},
                 {start: {$lt: endDate}}
@@ -65,7 +64,7 @@ module.exports = {
         return {result: entries}
     },
     findByEventId: async (eventId) => {
-        let relevantEntry = await ScheduleEntry.findOne({eventId: eventId});
+        const relevantEntry = await ScheduleEntry.findOne({eventId: eventId});
         if (!relevantEntry) {
             return {success: false}
         }
