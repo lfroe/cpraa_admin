3f964b405663b49a80192e7fa05903313ccdfbd9
Lukas Froeschl <lukas.froeschl@dig.at>
2017-08-31 08:58:24 +0200

    * try codeclimate coverage

--- specs/services/scheduleEntryServiceSpec.js
+++ specs/services/scheduleEntryServiceSpec.js
@@ -9,7 +9,7 @@ const rewire = require('rewire');
 const scheduleEntryService = rewire('../../app/services/scheduleEntryService');
 const utilServiceMock = {
     getLogger       : (logFileName, logPath) => {
-        console.log('logfilename is', logFileName)
+        console.log('logfilename is', logFileName);
         return {
             info: (logString) => {
             }
@@ -56,4 +56,22 @@ describe('scheduleEntry save', () => {
         expect(result.scheduleEntry).to.have.property('_id');
         assert.strictEqual(result.scheduleEntry.title, 'some title')
     });
+});
+describe('scheduleEntry save', () => {
+   it('try to delete schedule entry => success', async () => {
+       const requestData = {
+           user         : '5998619578bf532030c64981',
+           start        : new Date(),
+           end          : new Date(),
+           schoolClassId: '5777619578bf532030c64981',
+           testId       : '5998888878bf532030c64981',
+           title        : 'some title',
+           eventId      : '12345'
+       };
+       const result = await scheduleEntryService.saveScheduleEntry(requestData, {username: 'lufr', _id: '5998619578bf532030c64981'});
+       assert.isDefined(result.scheduleEntry);
+       await scheduleEntryService.deleteScheduleEntry(result.scheduleEntry._id);
+       const se = await ScheduleEntry.findOne({ _id: result.scheduleEntry._id });
+       assert.isNull(se);
+   })
 });
